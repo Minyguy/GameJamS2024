92153ed3224679c3a5898ce996140465fd3ca63e
Minyguy <benjaminleikvold@gmail.com>
2024-03-24 18:15:44 +0100

    Bullets now spawn properly

--- Bullet_script.gd
+++ Bullet_script.gd
@@ -3,9 +3,9 @@ extends RigidBody2D
 
 func _ready():
 	speed = 400
-	linear_velocity = Vector2(1,1).direction_to(get_global_mouse_position()-position)*speed
+	linear_velocity = get_parent().get_node("Player").position.direction_to(get_global_mouse_position()-position)*speed
 	print(linear_velocity)
-	position = get_parent().get_node("Player").position + (position.direction_to(get_global_mouse_position())*50)
+	position = get_parent().get_node("Player").position + (get_parent().get_node("Player").position.direction_to(get_global_mouse_position())*50)
 	print(linear_velocity)
 	print("THIS SHOULD WORK")
 	pass
