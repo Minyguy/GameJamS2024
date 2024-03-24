ef201825a17fab72d6255c509f58e5e87d046a74
Minyguy <benjaminleikvold@gmail.com>
2024-03-24 17:45:29 +0100

    thing

--- Bullet_script.gd
+++ Bullet_script.gd
@@ -5,7 +5,7 @@ func _ready():
 	speed = 400
 	
 	linear_velocity = Vector2(1,1).direction_to(get_global_mouse_position()-position)*speed
-	position = get_node(Player_CharacterBody2D).position
+	#position = get_node(Player_CharacterBody2D).position
 	print(linear_velocity)
 	print("THIS SHOULD WORK")
 	pass
