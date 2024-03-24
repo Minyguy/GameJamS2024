6940d3b18bafef4b43771f91cefc0919caaaf744
Minyguy <benjaminleikvold@gmail.com>
2024-03-24 18:18:12 +0100

    Removed redundant prints

--- Bullet_script.gd
+++ Bullet_script.gd
@@ -4,10 +4,7 @@ extends RigidBody2D
 func _ready():
 	speed = 400
 	linear_velocity = get_parent().get_node("Player").position.direction_to(get_global_mouse_position()-position)*speed
-	print(linear_velocity)
 	position = get_parent().get_node("Player").position + (get_parent().get_node("Player").position.direction_to(get_global_mouse_position())*50)
-	print(linear_velocity)
-	print("THIS SHOULD WORK")
 	pass
 	
 
