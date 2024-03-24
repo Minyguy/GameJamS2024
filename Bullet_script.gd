e0dcc8d682311f3758fbd59c78ad8f80eb3b64d8
Minyguy <benjaminleikvold@gmail.com>
2024-03-24 17:58:09 +0100

    stuff

--- Bullet_script.gd
+++ Bullet_script.gd
@@ -3,15 +3,13 @@ extends RigidBody2D
 
 func _ready():
 	speed = 400
-	
 	linear_velocity = Vector2(1,1).direction_to(get_global_mouse_position()-position)*speed
-	#position = get_node(Player_CharacterBody2D).position
+	print(linear_velocity)
+	position = get_parent().get_node("Player").position + (position.direction_to(get_global_mouse_position())*50)
 	print(linear_velocity)
 	print("THIS SHOULD WORK")
 	pass
 	
-func aim_at_mouse():
-	linear_velocity = Vector2(1,1).direction_to(get_global_mouse_position()-position)*speed
 
 	
 
