f58a9f6c373dc8334a79b152b1dadfb0b4d5cb8f
Minyguy <benjaminleikvold@gmail.com>
2024-04-05 18:07:24 +0200

    You can now die if you touch an enemy

--- Scripts/PlayerScript.gd
+++ Scripts/PlayerScript.gd
@@ -7,6 +7,7 @@ const JUMP_VELOCITY = 1000
 var base_shoot_cd = 30
 var curr_shoot_cd = 0
 var mouse = Vector2(0,0)
+var alive = true
 @onready var bullet_path = preload("res://Subscenes/Bullet.tscn")
 
 
@@ -17,36 +18,44 @@ func shoot_bullet(direction, speed):
 	add_sibling(bulletInst)
 
 func _process(delta):
-	if not curr_shoot_cd:
-		if Input.is_action_pressed("shoot"):
-			print(get_global_mouse_position())
-			shoot_bullet(get_global_mouse_position(),100)
-			curr_shoot_cd = base_shoot_cd
-	else:
-		curr_shoot_cd = move_toward(curr_shoot_cd, 0, 1)    
-		
-		
+	if (alive):
+		if not curr_shoot_cd:
+			if Input.is_action_pressed("shoot"):
+				print(get_global_mouse_position())
+				shoot_bullet(get_global_mouse_position(),100)
+				curr_shoot_cd = base_shoot_cd
+		else:
+			curr_shoot_cd = move_toward(curr_shoot_cd, 0, 1)    
+			
+			
 func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	
-	if not is_on_floor() and Input.is_action_just_pressed("jump"):
+	if not is_on_floor() and Input.is_action_pressed("jump") and alive:
 		velocity.y += gravity * delta - FLOAT_MOD * delta
 	elif not is_on_floor():
 		velocity.y += gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor() and alive:
 		print("JUMP")
 		velocity.y = -JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
+	if direction and alive:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+	for index in get_slide_collision_count():
+		var collisionStuff := get_slide_collision(index)
+		var body := collisionStuff.get_collider()
+		if body.name == "BasicEnemy":
+			print("YOU DIE!!!!")
+			alive = false
